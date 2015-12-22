e773284024441ddf95725e41f6c3fe3d071537d2
Ling Ma <malingreal@gmail.com>
2015-12-22 10:12:19 +0000

    clean code

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import requests
+import os
+from OCC.BRepTools import breptools_Read
+from OCC.TopoDS import TopoDS_Shape
+from OCC.BRep import BRep_Builder
+from OCC.Visualization import Tesselator
+
+app = Flask(__name__)
+app.config['APP_TEMP_FILE'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+#parse the ifc file and render it
+@app.route('/viewer/<entity_id>')
+def brep_viewer(entity_id):
+  if entity_id=="home":
+    # TODO: write code...
+    return render_template('viewer.html');
+#   entity_id="56634e77180a4ae67618684b"
+  r = requests.get('http://restfulifc.herokuapp.com/geometry?where={"entity_id": "'+entity_id+'"}')
+  response=r.json()
+  brep=''
+  if '_items' in response.keys():
+      item=response['_items'][0]
+      brep=item['occ_brep'].encode('ascii')
+  else:
+      brep=response['occ_brep'].encode('ascii')
+#   brep=item['occ_brep'].encode('ascii')
+  path_brep = os.path.join(app.config['APP_TEMP_FILE'], entity_id+".brep")
+  path_js = os.path.join(app.config['APP_TEMP_FILE'], entity_id+".js")
+  with open(path_brep, 'wb') as f:
+      f.write(brep)
+  f.close()
+  get_tess(path_brep,path_js)
+#   return path_js
+  return render_template('viewer.html',js_filename=entity_id+".js")
+
+def get_tess(brep_filename,js_filename):
+  shape = TopoDS_Shape()
+  builder = BRep_Builder()
+  breptools_Read(shape, str(brep_filename), builder)
+  tess = Tesselator(shape)
+  tess.ExportShapeToJSON(str(js_filename))
+
+if __name__ == "__main__":
+    # app.run()
+    
+    app.run(host=os.environ['IP'],port=8081,debug=True)
+    # app.run(host=os.environ['IP'],port=int(os.environ['PORT']),debug=True)
